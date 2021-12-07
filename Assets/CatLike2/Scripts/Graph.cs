ff9f7c4855befd2cabeb004d4c9e479257712561
HungryHighness <HungryHighness@outlook.com>
2021-12-08 03:47:01 +0800

    开始catlike3

--- Assets/CatLike2/Scripts/Graph.cs
+++ Assets/CatLike2/Scripts/Graph.cs
@@ -1,45 +1,46 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Graph : MonoBehaviour
+namespace CatLike2.Scripts
 {
-    [SerializeField] private Transform pointPrefab;
-
-    [SerializeField, Range(10, 100)] private int resolution = 10;
-
-    private Transform[] _points;
-    private float _step;
-    private Vector3 _scale;
-    private Vector3 _position;
-
-    // Start is called before the first frame update
-    void Start()
+    public class Graph : MonoBehaviour
     {
-        _step = 2f / resolution;
-        _scale = Vector3.one * _step;
-        _position = Vector3.one;
-        _points = new Transform[resolution];
+        [SerializeField] private Transform pointPrefab;
 
-        for (var i = 0; i < _points.Length; i++)
+        [SerializeField, Range(10, 100)] private int resolution = 10;
+
+        private Transform[] _points;
+        private float _step;
+        private Vector3 _scale;
+        private Vector3 _position;
+
+        // Start is called before the first frame update
+        void Start()
         {
-            _points[i] = Instantiate(pointPrefab, transform, false);
-            _position.x = (i + 0.5f) * _step - 1f;
-            // _position.y = _position.x * _position.x * _position.x;
-            _points[i].localPosition = _position;
-            _points[i].localScale = _scale;
+            _step = 2f / resolution;
+            _scale = Vector3.one * _step;
+            _position = Vector3.one;
+            _points = new Transform[resolution];
+
+            for (var i = 0; i < _points.Length; i++)
+            {
+                _points[i] = Instantiate(pointPrefab, transform, false);
+                _position.x = (i + 0.5f) * _step - 1f;
+                // _position.y = _position.x * _position.x * _position.x;
+                _points[i].localPosition = _position;
+                _points[i].localScale = _scale;
+            }
         }
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        float time = Time.time;
-        for (var i = 0; i < _points.Length; i++)
+        // Update is called once per frame
+        void Update()
         {
-            _position = _points[i].position;
-            _position.y = Mathf.Sin(Mathf.PI * (_position.x + time));
-            _points[i].localPosition = _position;
+            float time = Time.time;
+            for (var i = 0; i < _points.Length; i++)
+            {
+                _position = _points[i].position;
+                _position.y = Mathf.Sin(Mathf.PI * (_position.x + time));
+                _points[i].localPosition = _position;
+            }
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # CatLike
+
+跟随CatLike教程的记录。
+
+附上Catlike链接：
+
+https://catlikecoding.com/
