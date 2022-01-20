61c9f7594b3b2e96a18d35d4b082a05165a92c1e
HungryHighness <HungryHighness@outlook.com>
2022-01-20 20:13:28 +0800

    自定义重力

---
+++ Assets/Movement/ComplexGravity/CustomGravity.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Movement.ComplexGravity
+{
+    public class CustomGravity
+    {
+        private static List<GravitySource> _sources = new List<GravitySource>();
+
+        public static Vector3 GetGravity(Vector3 position)
+        {
+            Vector3 g = Vector3.zero;
+            for (var i = 0; i < _sources.Count; i++)
+            {
+                g += _sources[i].GetGravity(position);
+            }
+
+            return g;
+        }
+
+        public static Vector3 GetUpAxis(Vector3 position)
+        {
+            Vector3 g = Vector3.zero;
+            for (var i = 0; i < _sources.Count; i++)
+            {
+                g += _sources[i].GetGravity(position);
+            }
+
+            return -g.normalized;
+        }
+
+        public static Vector3 GetUpAxis(Vector3 position, out Vector3 upAxis)
+        {
+            Vector3 g = Vector3.zero;
+            for (var i = 0; i < _sources.Count; i++)
+            {
+                g += _sources[i].GetGravity(position);
+            }
+
+            upAxis = -g.normalized;
+            return g;
+        }
+
+        public static void Register(GravitySource source)
+        {
+            Debug.Assert(!_sources.Contains(source), "重力源重复注册!", source);
+            _sources.Add(source);
+        }
+
+        public static void Unregister(GravitySource source)
+        {
+            Debug.Assert(_sources.Contains(source), "未注册的未知重力源!", source);
+            _sources.Remove(source);
+        }
+
+
+    }
+}
---
+++ Assets/Movement/ComplexGravity/GravitySource.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace Movement.ComplexGravity
+{
+    public class GravitySource : MonoBehaviour
+    {
+        public Vector3 GetGravity(Vector3 position)
+        {
+            return Physics.gravity;
+        }
+
+        private void OnEnable()
+        {
+            CustomGravity.Register(this);
+        }
+
+        private void OnDisable()
+        {
+            CustomGravity.Unregister(this);
+        }
+    }
+}
